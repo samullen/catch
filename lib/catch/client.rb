c9756edf56c8c2652aaccc6bf2813c90c324a660
Samuel Mullen <samullen@gmail.com>
2011-05-03 13:26:19 -0500

    client uses faraday_middleware and includes Note.

--- lib/catch/client.rb
+++ lib/catch/client.rb
@@ -1,16 +1,19 @@
 module Catch
   class Client
+    include Note
 
     def initialize(options={})
       @api_url = "https://api.catch.com/v1"
       username = options[:username]
       password = options[:password]
+      connection.basic_auth(username, password)
     end
 
     def connection
       @connection ||= Faraday.new(:url => @api_url, :headers => default_headers) do |builder|
-#         builder.use Faraday::Request::JSON
         builder.adapter Faraday.default_adapter
+        builder.use Faraday::Response::Mashify
+        builder.use Faraday::Response::ParseJson
       end
     end
 
