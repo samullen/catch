43dcdb34cb61b3b8147aa8232c0e186ff2add27e
Samuel Mullen <samullen@gmail.com>
2011-05-05 07:58:58 -0500

    note and notes return just the relevant hash.

--- lib/catch/note.rb
+++ lib/catch/note.rb
@@ -1,11 +1,11 @@
 module Catch
   module Note
     def notes
-      connection.get("notes").body
+      connection.get("notes").body.notes
     end
 
     def note(id)
-      connection.get("notes/#{id}").body
+      connection.get("notes/#{id}").body.notes.first
     end
   end
 end
