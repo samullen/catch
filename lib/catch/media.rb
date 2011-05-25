6235bf307c46f6443ebd95de46d8969a94101b19
Samuel Mullen <samullen@gmail.com>
2011-05-25 07:38:24 -0500

    add comments for why we're not using shared_media API method.

--- lib/catch/media.rb
+++ lib/catch/media.rb
@@ -18,6 +18,10 @@ module Catch
       connection.delete("media/#{note_id}/#{media_id}").body.status == 'ok'
     end
 
+    # It really doesn't look like shared_media has any value at this point
+    # since all it is doing is retrieving a file from the url key of the media
+    # object. Skipping until this has value.
+
 #     def shared_media(note_id, media_id, params={})
 #       media_connection.get do |req|
 #         req.url("media/#{note_id}/#{media_id}")
