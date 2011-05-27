e95528be63d9870240482b73b1c2393b6a398035
Samuel Mullen <samullen@gmail.com>
2011-05-27 07:22:38 -0500

    add documentation to tag module.

--- lib/catch/tag.rb
+++ lib/catch/tag.rb
@@ -1,5 +1,10 @@
 module Catch
   module Tag
+
+    # Provide a list of all tags in this user's notes. The response provides
+    # count (number of notes using this tag) and modified (most recent
+    # timestamp when a note using this tag was modified) properties for each
+    # tag
     def tags
       connection.get("tags").body.tags
     end
